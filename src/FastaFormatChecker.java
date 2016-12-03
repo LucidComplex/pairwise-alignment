48c189f60c3f1e0bd316255d2a3aaa345c4a5ee5
Eli Tan <eliedrian1996@gmail.com>
2016-12-04 01:20:32 +0800

    Implement checking nucleotide or protein sequence

--- src/FastaFormatChecker.java
+++ src/FastaFormatChecker.java
@@ -5,15 +5,23 @@ import java.util.Arrays;
  * Created by tan on 12/3/16.
  */
 public class FastaFormatChecker {
+    public static final int PROTEIN = 1;
+    public static final int NUCLEOTIDE = 2;
 
-    public static boolean check(String fasta) {
+    public static boolean check(int type, String fasta) {
         String[] fastaLines = fasta.split("\n");
         if (!fastaLines[0].startsWith(">")) {
             return false;
         } else {
             for (int i = 1; i < fastaLines.length; i++) {
-                if (!fastaLines[i].matches("(A|C|T|G)+")) {
-                    return false;
+                if (type == PROTEIN) {
+                    if (!fastaLines[i].matches("[ARNDCEQGHILKMFPSTWYV]+")) {
+                        return false;
+                    }
+                } else if (type == NUCLEOTIDE) {
+                    if (!fastaLines[i].matches("[ACTG]+")) {
+                        return false;
+                    }
                 }
             }
         }
--- tests/FastaFormatCheckerTest.java
+++ tests/FastaFormatCheckerTest.java
@@ -9,12 +9,24 @@ public class FastaFormatCheckerTest {
     @Test
     public void checkFormat() {
         String validFasta = "> PROTEIN ID | TEST SEQUENCE\nACTGATCGGATCGATACA";
-        assertTrue(FastaFormatChecker.check(validFasta));
+        assertTrue(FastaFormatChecker.check(FastaFormatChecker.NUCLEOTIDE, validFasta));
     }
 
     @Test
     public void invalidFormat() {
         String invalidFasta = "> PROTEIN \n\nACTGAC";
-        assertFalse(FastaFormatChecker.check(invalidFasta));
+        assertFalse(FastaFormatChecker.check(FastaFormatChecker.NUCLEOTIDE, invalidFasta));
+    }
+
+    @Test
+    public void checkProtein() {
+        String validProtein = "> PROTEIN\nVFTELSPAKTV";
+        assertTrue(FastaFormatChecker.check(FastaFormatChecker.PROTEIN, validProtein));
+    }
+
+    @Test
+    public void invalidProtein() {
+        String validProtein = "> PROTEIN\nVFTULSPAKTV";
+        assertFalse(FastaFormatChecker.check(FastaFormatChecker.PROTEIN, validProtein));
     }
 }
