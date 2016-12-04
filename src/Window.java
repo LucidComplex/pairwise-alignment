10c7941cde3eac55c53fdf9083d10702612cc590
Eli Tan <eliedrian1996@gmail.com>
2016-12-05 03:07:23 +0800

    Create ui and and blosum62 data

---
+++ src/Window.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+/**
+ * Created by tan on 12/5/16.
+ */
+public class Window {
+    private JButton uploadFASTAButton;
+    private JTextArea textArea1;
+    private JButton clearButton;
+    private JRadioButton nucleotideRadioButton;
+    private JRadioButton proteinRadioButton;
+    private JButton alignButton;
+    private JComboBox comboBox1;
+}
