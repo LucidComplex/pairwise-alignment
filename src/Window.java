b58d5615e308ffc068c05ea2b36fff7680590a9b
Eli Tan <eliedrian1996@gmail.com>
2016-12-05 13:23:35 +0800

    Update UI

--- src/Window.java
+++ src/Window.java
@@ -1,16 +1,36 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by tan on 12/5/16.
  */
 public class Window {
     private JButton uploadFASTAButton;
-    private JTextArea textArea1;
+    private JTextArea fastaInput;
     private JButton clearButton;
     private JRadioButton nucleotideRadioButton;
     private JRadioButton proteinRadioButton;
     private JComboBox comboBox1;
     private JButton alignButton;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
     private ButtonGroup inputType;
 
+    public Window() {
+        clearButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                fastaInput.setText("");
+            }
+        });
+        uploadFASTAButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fileChooser = new JFileChooser();
+                fileChooser.showOpenDialog(uploadFASTAButton);
+            }
+        });
+    }
 }
