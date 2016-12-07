6531982634d7f2ef1d0922184ed1d650b91394b7
Eli Tan <eliedrian1996@gmail.com>
2016-12-07 23:49:51 +0800

    Make progress on traceBack

--- src/SimpleAligner.java
+++ src/SimpleAligner.java
@@ -1,4 +1,9 @@
 import fasta.Fasta;
+import sun.reflect.generics.tree.Tree;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
 
 /**
  * Created by rl-14 on 12/5/16.
@@ -18,20 +23,103 @@ public class SimpleAligner {
         String sequenceA = f1.getSequence();
         String sequenceB = f2.getSequence();
         int[][] matrix = SequenceAligner.createGlobalMatrix(sequenceA.length(), sequenceB.length(), gap);
-        Direction[][] directions = fillMatrix(matrix, sequenceA, sequenceB);
+        Path[][] paths = fillMatrix(matrix, sequenceA, sequenceB);
+        for (int i = 1; i < paths.length; i++) {
+            for (int j = 1; j < paths[0].length; j++) {
+                if (paths[i][j].diagonal) {
+                    System.out.printf("d");
+                }
+                else {
+                    System.out.printf(".");
+                }
+                if (paths[i][j].left) {
+                    System.out.printf("l");
+                }
+                else {
+                    System.out.printf(".");
+                }
+                if (paths[i][j].up) {
+                    System.out.printf("u");
+                }
+                else {
+                    System.out.printf(".");
+                }
+                System.out.printf(" ");
+            }
+            System.out.println();
+        }
+        TreeNode node = new TreeNode();
+        traceBack(paths, paths.length - 1, paths[0].length - 1, node.children);
+        printAlignment(node);
         int score = matrix[matrix.length - 1][matrix[0].length - 1];
         return score;
     }
 
-    public Direction[][] fillMatrix(int[][] matrix, String seqA, String seqB) {
-        Direction[][] directions = new Direction[matrix.length][matrix[0].length];
-        Direction dir;
+    private void printAlignment(TreeNode node) {
+        Stack<TreeNode> stack = new Stack<>();
+        Stack<String> alignment = new Stack<>();
+        TreeNode next;
+        stack.push(node);
+        while (!stack.isEmpty()) {
+            next = stack.pop();
+            for (TreeNode n : next.children) {
+                stack.push(n);
+            }
+            if (next.direction == null) {
+                continue;
+            }
+            switch (next.direction) {
+                case DIAGONAL:
+                    alignment.push("DIAG");
+                    break;
+                case LEFT:
+                    alignment.push("LEFT");
+                    break;
+                case UP:
+                    alignment.push("UP");
+                    break;
+            }
+        }
+        while (!alignment.isEmpty()) {
+            System.out.printf(alignment.pop() + " ");
+        }
+    }
+
+    private void traceBack(Path[][] paths, int row, int col, List<TreeNode> children) {
+        Path dir = paths[row][col];
+        TreeNode n;
+        if (row == 0 && col == 0) {
+            return;
+        }
+        if (dir == null) {
+            return;
+        }
+        if (dir.diagonal) {
+            n = new TreeNode(Direction.DIAGONAL);
+            children.add(n);
+            traceBack(paths, row - 1, col - 1, n.children);
+        }
+        if (dir.left) {
+            n = new TreeNode(Direction.LEFT);
+            children.add(n);
+            traceBack(paths, row, col - 1, n.children);
+        }
+        if (dir.up) {
+            n = new TreeNode(Direction.UP);
+            children.add(n);
+            traceBack(paths, row - 1, col, n.children);
+        }
+    }
+
+    public Path[][] fillMatrix(int[][] matrix, String seqA, String seqB) {
+        Path[][] paths = new Path[matrix.length][matrix[0].length];
+        Path dir;
         for (int i = 1; i < matrix.length; i++) {
             for (int j = 1; j < matrix[i].length; j++) {
-                dir = new Direction();
+                dir = new Path();
                 int diagonal = matrix[i - 1][j - 1] + score(seqA.charAt(i - 1), seqB.charAt(j - 1));
-                int up = matrix[i][j - 1] + gap;
-                int left = matrix[i - 1][j] + gap;
+                int up = matrix[i - 1][j] + gap;
+                int left = matrix[i][j - 1] + gap;
                 int max = diagonal;
                 if (max < up) {
                     max = up;
@@ -49,11 +137,11 @@ public class SimpleAligner {
                     dir.up = true;
                 }
 
-                directions[i][j] = dir;
+                paths[i][j] = dir;
                 matrix[i][j] = max;
             }
         }
-        return directions;
+        return paths;
     }
 
     private int score(char a, char b) {
@@ -62,6 +150,24 @@ public class SimpleAligner {
 
 }
 
-class Direction {
+class Path {
     boolean up, left, diagonal;
+}
+
+enum Direction {
+    UP, LEFT, DIAGONAL;
+}
+
+class TreeNode {
+    Direction direction;
+    List<TreeNode> children;
+
+    public TreeNode() {
+        children = new LinkedList<>();
+    }
+
+    public TreeNode(Direction direction) {
+        this();
+        this.direction = direction;
+    }
 }
--- tests/SimpleAlignerTest.java
+++ tests/SimpleAlignerTest.java
@@ -24,7 +24,7 @@ public class SimpleAlignerTest {
         Fasta f1 = new Fasta(Fasta.NUCLEOTIDE, "PROTEIN", "GGATCGA");
         Fasta f2 = new Fasta(Fasta.NUCLEOTIDE, "PROTEIN", "GAATTCAGTTA");
         int[][] matrix = SequenceAligner.createGlobalMatrix(f1.getSequence().length(), f2.getSequence().length(), -4);
-        Direction[][] directions = aligner.fillMatrix(matrix, f1.getSequence(), f2.getSequence());
+        Path[][] paths = aligner.fillMatrix(matrix, f1.getSequence(), f2.getSequence());
         assertEquals(11, matrix[7][11]);
         assertEquals(6, matrix[6][10]);
     }
